631b060dcded0084499f1378dc7177b0b9d6d92c
Alberto-Ionut Toscariu <62894046+ElusiveMind22@users.noreply.github.com>
2021-04-19 20:50:34 +0300

    Javascript1
    Javascript1 plus niste modificari css

--- continut/js/script.js
+++ continut/js/script.js
@@ -1,3 +1,4 @@
+
 function funct1()
 {
     
@@ -13,13 +14,24 @@ var yyyy = today.getFullYear();
 today = dd + '/' + mm + '/' + yyyy;
 time = hh+":"+minmin+":"+ss;
 
+if(navigator.geolocation)
+{
+    navigator.geolocation.getCurrentPosition(function(position)
+    {
+    document.getElementById("locatia").innerHTML = "Latitude: " + position.coords.latitude +" Longitude: " + position.coords.longitude;
+
+    })
+}
+else {
+    document.getElementById("locatia").innerHTML = "Geolocation is not supported by this browser.";
+  }
+
 document.getElementById('data').innerHTML = today;
 document.getElementById('ora').innerHTML = time;
 document.getElementById('adresaUrl').innerHTML=window.location.href;
 document.getElementById('NBrowser').innerHTML=navigator.appName;
 document.getElementById('VBrowser').innerHTML=navigator.appVersion;
 document.getElementById('OS').innerHTML=navigator.platform;
-document.getElementById('location').innerHTML=navigator.platform;
 
 
 }
